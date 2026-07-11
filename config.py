332b0dabb84e9b02c6f14b93ac51194c1d000b62
GeraldM-007 <geraldmuchuku237@gmail.com>
2026-07-11 23:26:42 +0300

    contains header, url and pages

---
+++ config.py
@@ -0,0 +1,8 @@
+#user agent to identify where the request is coming from(browser not a bot )
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+#url for the site to be scrapped
+url = "https://ke.oraimo.com/"
+
+#oraimo product pages 
+pages = 15
